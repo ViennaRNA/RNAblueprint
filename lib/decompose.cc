b739a51dbbd449fda51a6f22af5ad8fb5f1970ef
Stefan Hammer <s.hammer@univie.ac.at>
2015-11-12 13:46:52 +0100

    ear decomposition optimization now done

--- lib/decompose.cc
+++ lib/decompose.cc
@@ -78,7 +78,7 @@ namespace design {
                 // this is a block
                 } else {
                     // do the ear decomposition and create to subgraphs
-                    ear_decomposition_to_subgraphs(g, rand);
+                    ear_decomposition_to_subgraphs(g, rand, true);
 
                     if (debug) {
                         std::cerr << "subgraphs ear decomposition:" << std::endl;
@@ -242,7 +242,7 @@ namespace design {
         }
 
         template <typename RG>
-        void ear_decomposition_to_subgraphs(Graph& g, RG& rand) {
+        void ear_decomposition_to_subgraphs(Graph& g, RG& rand, bool optimize_decomposition) {
             // blocks need to be decomposed into path. this can be done by Ear Decomposition
             
             // ear map (this is what we want to fill!)
@@ -250,17 +250,26 @@ namespace design {
             
             //best pred map
             boost::vector_property_map<Vertex> best_pred(boost::num_vertices(g));
-            int best_alpha, best_beta = std::numeric_limits<int>::max();
+            // container to store attachment points
+            std::vector<Vertex> att_points;
+            
+            int best_alpha = std::numeric_limits<int>::max();
+            int best_beta = std::numeric_limits<int>::max();
             // TODO think of a better way to get the best spanning tree. 
             // for now do a stupid optimization
-            for (int i =0; i < 1000; i++) {
-                // container to store attachment points
-                std::vector<Vertex> att_points;
+            int count = 0;
+            while (true) {
                 // predecessor map
                 boost::vector_property_map<Vertex> pred(boost::num_vertices(g));
                 // get a random spanning tree
                 boost::random_spanning_tree(g, rand, boost::predecessor_map(pred));
-                
+                // if no optimization should be done, exit here
+                if (!optimize_decomposition) {
+                    if (debug)
+                        std::cerr << "No spanning tree and ear decomposition optimization!" << std::endl;
+                    best_pred = pred;
+                    break;
+                }
                 // run the ear decomposition
                 int num = boost::ear_decomposition(g, pred, em, std::back_inserter(att_points));
                 
@@ -270,18 +279,29 @@ namespace design {
                         std::cout << "(" << boost::source(e, g) << "/" << boost::target(e, g) << "): " << g[e].ear << std::endl;
                     }
                 }
+                // calculate alpha and beta
                 int alpha, beta;
                 boost::tie(alpha, beta) = get_alpha_beta(g, att_points, num);
-                //std::cerr << alpha << "/" << beta << std::endl;
+                // clean up att_points
+                att_points.clear();
+                
                 if ((alpha < best_alpha && beta <= best_beta) || (beta < best_beta && alpha <= best_alpha)) {
                     best_alpha = alpha;
                     best_beta = beta;
                     best_pred = pred;
+                    count = 0;
+                    if (debug)
+                        std::cerr << "Better Solution: " << best_alpha << "/" << best_beta << std::endl;
+                } else {
+                    count++;
+                    if (debug)
+                        std::cerr << "Optimization Count: " << count << " - " << alpha << "/" << beta << std::endl;
+                    // optimization exit
+                    if (count > 10)
+                        break;
                 }
             }
             // redo best ear decomposition
-            // container to store attachment points
-            std::vector<Vertex> att_points;
             // run the ear decomposition with best_pred
             if (debug) {
                 std::cerr << "Best alpha/beta: " << best_alpha << "/" << best_beta << std::endl;
@@ -321,7 +341,9 @@ namespace design {
             // map to store all attachment points ordered by ear index
             // A[k] = { vertex, vertex, vertex }
             std::map<int, std::vector<Vertex> > Ak;
-            
+            Ak[0] = {}; // this is important, because for the first and last ear we have no attachment points
+            Ak[num] = {};
+            // fill the map 
             for (Vertex v : att_points) {
                 int min_ear_index = std::numeric_limits<int>::max();
                 int max_ear_index = 0;
@@ -334,8 +356,8 @@ namespace design {
                 for (int k = min_ear_index; k < max_ear_index; k++)
                     Ak[k].push_back(v);
             }
-            Ak[0] = {}; // this is important, because for the first and last ear we have no attachment points
-            Ak[num] = {};
+            if (debug)
+                std::cerr << Ak << std::endl;
             
             for (int k = 0; k < num-1; ++k) {
                 // for making set_difference we need a sorted vector (Ak[0] is empty and therfore already sorted)
@@ -345,6 +367,8 @@ namespace design {
                 std::vector<Vertex>::iterator c_it = std::set_difference(Ak[k+1].begin(), Ak[k+1].end(), Ak[k].begin(), Ak[k].end(), complement.begin());
                 complement.resize(c_it-complement.begin());
                 
+                if (debug)
+                    std::cerr << complement << std::endl;
                 // calculate alpha = max_k(|Ak|)
                 int current_alpha = Ak[k].size();
                 if (current_alpha > alpha)
@@ -422,6 +446,6 @@ namespace design {
         }
 
         template bool decompose_graph<std::mt19937> (Graph&, std::mt19937&);
-        template void ear_decomposition_to_subgraphs<std::mt19937> (Graph&, std::mt19937&);
+        template void ear_decomposition_to_subgraphs<std::mt19937> (Graph&, std::mt19937&, bool);
     }
 }
