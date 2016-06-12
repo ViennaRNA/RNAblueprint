1719e852c8e7b423ff38c5c583f0eca8d140733f
Stefan Hammer <s.hammer@univie.ac.at>
2016-06-12 09:18:23 +0200

    update documentation with examples and new interface for the next release

--- RNAdesign_8h.js
+++ RNAdesign_8h.js
@@ -2,6 +2,11 @@ var RNAdesign_8h =
 [
     [ "DependencyGraph", "classdesign_1_1DependencyGraph.html", "classdesign_1_1DependencyGraph" ],
     [ "graph_is_bipartite", "RNAdesign_8h.html#a07cad79644669673b0bbb917b4f72129", null ],
+    [ "incompatible_sequence_positions", "RNAdesign_8h.html#ad3e110e07eda2fa4ec637c1da57046a4", null ],
     [ "initialize_library", "RNAdesign_8h.html#a8fc0aec08efdb4455d2a7e1f620bf265", null ],
+    [ "initialize_library", "RNAdesign_8h.html#ae1d8b9638e7e2464fc2edc7f1b706cbe", null ],
+    [ "sequence_structure_compatible", "RNAdesign_8h.html#ad2ddc0596f71522e14e97ff2ce4530a8", null ],
+    [ "structures_to_graphml", "RNAdesign_8h.html#af3e51a64b79205f5c413cae3292912d6", null ],
+    [ "structures_to_graphml", "RNAdesign_8h.html#a5450a8bae9c437677a80bed71c95cdb9", null ],
     [ "structures_to_graphml", "RNAdesign_8h.html#a17fce75eddf08ea96fd3f59a70a88b4d", null ]
 ];
---
+++ navtreedata.js
@@ -0,0 +1,43 @@
+var NAVTREE =
+[
+  [ "RNAdesign-0.1", "index.html", [
+    [ "RNAdesign library written in C++", "index.html", [
+      [ "Introduction", "index.html#intro_sec", null ],
+      [ "Dependencies", "index.html#dependency_sec", null ],
+      [ "Installation", "index.html#install_sec", null ],
+      [ "Interface Examples", "index.html#examples_sec", [
+        [ "Python example", "index.html#python_ex", null ],
+        [ "Perl example", "index.html#perl_ex", null ],
+        [ "C++ example", "index.html#cpp_ex", null ]
+      ] ],
+      [ "How to cite", "index.html#cite_sec", null ]
+    ] ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"RNAdesign_8h.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
