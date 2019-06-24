6eecd02edca2940fbf531f23f99483b1c7dae566
Sven Findeiss <sven@bioinf.uni-leipzig.de>
2019-06-24 16:29:41 +0200

    update documentation to 1.3

--- navtreedata.js
+++ navtreedata.js
@@ -1,29 +1,6 @@
-/*
-@ @licstart  The following is the entire license notice for the
-JavaScript code in this file.
-
-Copyright (C) 1997-2017 by Dimitri van Heesch
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-@licend  The above is the entire license notice
-for the JavaScript code in this file
-*/
 var NAVTREE =
 [
-  [ "RNAblueprint-1.2.3", "index.html", [
+  [ "RNAblueprint-1.3.0", "index.html", [
     [ "RNAblueprint library", "index.html", [
       [ "Introduction", "index.html#intro_sec", null ],
       [ "Dependencies", "index.html#dependency_sec", null ],
@@ -37,8 +14,8 @@ var NAVTREE =
       [ "How to cite", "index.html#cite_sec", null ]
     ] ],
     [ "Bug List", "bug.html", null ],
-    [ "Namespaces", "namespaces.html", [
-      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
       [ "Namespace Members", "namespacemembers.html", [
         [ "All", "namespacemembers.html", null ],
         [ "Functions", "namespacemembers_func.html", null ]
@@ -53,8 +30,8 @@ var NAVTREE =
         [ "Variables", "functions_vars.html", null ]
       ] ]
     ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ]
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
     ] ]
   ] ]
 ];
